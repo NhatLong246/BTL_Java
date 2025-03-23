5bd5038086aa38351bb579be1d945ec6d7e30d1e
NhatLong246 <trannhatlong240605@gmail.com>
2025-03-23 13:44:00 +0700

    m

--- src/entity/Insurance.java
+++ src/entity/Insurance.java
@@ -19,29 +19,4 @@ public class Insurance {
     public Insurance() {}
 
 
-
-
-    public String inputInsuranceId() {
-        Scanner scanner = new Scanner(System.in);
-        String insuranceId;
-        String regex = "^[A-Z]{2}[1-5]\\d{2}\\d{10}$"; // Định dạng mã BHYT
-
-        while (true) {
-            System.out.print("Nhập mã bảo hiểm y tế (hoặc nhấn Enter nếu không có): ");
-            insuranceId = scanner.nextLine().trim();
-
-            if (insuranceId.isEmpty()) {
-                System.out.println("Bạn đã chọn không có bảo hiểm.");
-                return "Không có bảo hiểm"; // Hoặc có thể là null
-            }
-
-            if (insuranceId.matches(regex)) {
-                return insuranceId; // Mã hợp lệ
-            } else {
-                System.out.println("⚠️ Mã bảo hiểm không hợp lệ! Hãy nhập lại theo định dạng DN-2-10-1234567890.");
-            }
-        }
-    }
-
-
 }
